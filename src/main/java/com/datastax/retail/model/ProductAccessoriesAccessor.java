ac7c4d44f5eadd731df63cec16f3f8082521a053
vincentponcet <vincent.poncet@gmail.com>
2016-04-14 14:52:58 +0200

    remove Async methods. Not used.

--- src/main/java/com/datastax/retail/model/ProductAccessoriesAccessor.java
+++ src/main/java/com/datastax/retail/model/ProductAccessoriesAccessor.java
@@ -4,15 +4,11 @@ import com.datastax.driver.mapping.Result;
 import com.datastax.driver.mapping.annotations.Accessor;
 import com.datastax.driver.mapping.annotations.Param;
 import com.datastax.driver.mapping.annotations.Query;
-import com.google.common.util.concurrent.ListenableFuture;
 
 @Accessor
 public interface ProductAccessoriesAccessor {
 
 	 @Query("SELECT * FROM product_accessories WHERE solr_query= :solr_query limit 100")
 	    public Result<ProductAccessories> getAll(@Param("solr_query") java.lang.String solr_query);
-	 
-	 @Query("SELECT * FROM product_accessories WHERE solr_query= :solr_query limit 100")
-	 public ListenableFuture<Result<ProductAccessories>> getAllAsync(@Param("solr_query") java.lang.String solr_query);
 
 }
