e94997e042ed2451bcfed36f8e147c94b70b0d8b
Thom Valley <thomvalley@gmail.com>
2016-05-02 13:02:51 -0400

    Updates to product detail page (add recommendations) not pretty yet, but functional

--- src/main/webapp/html/js/product.js
+++ src/main/webapp/html/js/product.js
@@ -5,9 +5,6 @@ function updateProduct(sku){
         url: '/datastax-starter/rest/getProductsSolrQuery/sku:' + sku,
         success: function(dataset) {
             var elementCount = 0;
-            //Clear all HTML inside product table
-
-            elementHtml = "";
 
             //$("#product-grid").append("<div class='col-sm-6 col-md-6 col-lg-4' id='product-rowb'>")
 
@@ -24,8 +21,38 @@ function updateProduct(sku){
         },
         cache: false
     });
+
 }
 
+function updateRecommendations(sku){
+
+    $.ajax({
+        url: '/datastax-starter/rest/getRecommendedProductsBySku/' + sku,
+        success: function(dataset) {
+            elementHtml = "";
+            //$("#product-grid").append("<div class='col-sm-6 col-md-6 col-lg-4' id='product-rowb'>")
+
+
+                $.each(dataset, function (index, element) {
+                    //increment row-element count
+                    elementHtml += "<div class='col-sm-4 col-md-4 col-lg-3' id='product-row'>";
+                    elementHtml += "<div class='product-content product-wrap clearfix'>";
+                    elementHtml += element.productName;
+                    elementHtml += "<div><div class='col-lg-1'>$" + element.regularPrice;
+                    elementHtml += "<img class='img-responsive' src='" + element.thumbnailImage + "'></div>";
+                    elementHtml += "</div>";
+                    elementHtml += "</div>";
+                    elementHtml += "</div>";
+
+                    return (index != 7);
+
+                });
+
+            $("#recommendations").append(elementHtml);
+        },
+        cache: false
+    });
+}
 
 function getQueryVariable(variable) {
     var query = window.location.search.substring(1);
@@ -41,7 +68,4 @@ function getQueryVariable(variable) {
 
 
 updateProduct(getQueryVariable("sku"));
-
-
-
-
+updateRecommendations(getQueryVariable("sku"));
